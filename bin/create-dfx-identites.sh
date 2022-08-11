99ecb183fc90a98afc9cb3985a9d442cdd5a391c
Max <max@dfinity.org>
2022-08-11 11:40:02 +0200

    fmt

--- bin/create-dfx-identites.sh
+++ bin/create-dfx-identites.sh
@@ -5,21 +5,19 @@ DFX_IDENT_DIR="$HOME/.config/dfx/identity"
 
 cd "$(dirname $0)"
 
-for I in {1..4}
-do
-	echo "Creating dfx identity 'ident-$I' at $DFX_IDENT_DIR/ident-$I/identity.pem"
-	rm -rf "$DFX_IDENT_DIR/ident-$I"
-	mkdir -p "$DFX_IDENT_DIR/ident-$I"
-	cp ../identities/identity-$I.pem "$DFX_IDENT_DIR/ident-$I/identity.pem"
-	sed -i -e 1,3d "$DFX_IDENT_DIR/ident-$I/identity.pem" 
-	dfx identity use ident-$I
-	PRINCIPAL_ID=$(dfx identity get-principal)
-	ACCOUNT_ID=$(dfx ledger account-id)
-	echo "PrincipalId: $PRINCIPAL_ID" 
-	echo "AccountId: $ACCOUNT_ID"
+for I in {1..4}; do
+  echo "Creating dfx identity 'ident-$I' at $DFX_IDENT_DIR/ident-$I/identity.pem"
+  rm -rf "$DFX_IDENT_DIR/ident-$I"
+  mkdir -p "$DFX_IDENT_DIR/ident-$I"
+  cp ../identities/identity-$I.pem "$DFX_IDENT_DIR/ident-$I/identity.pem"
+  sed -i -e 1,3d "$DFX_IDENT_DIR/ident-$I/identity.pem"
+  dfx identity use ident-$I
+  PRINCIPAL_ID=$(dfx identity get-principal)
+  ACCOUNT_ID=$(dfx ledger account-id)
+  echo "PrincipalId: $PRINCIPAL_ID"
+  echo "AccountId: $ACCOUNT_ID"
 
-	echo ""
+  echo ""
 done
 
 dfx identity use ident-1
-
