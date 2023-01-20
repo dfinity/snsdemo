9e10edcfb6684d908330881751bbcbb16d98c780
Max Murphy-Skvorzov <max@winning.black>
2023-01-20 14:22:19 +0100

    ++

--- bin/versions.bash
+++ bin/versions.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034 # VAriables are expected to be unused in this file
-DFX_IC_COMMIT=82e0a665b380878ad1d63c13d6d2deb0296cab7b
+DFX_IC_COMMIT=14a1e25580baad292c0b714de2427e2eb9922e3c
