e54d9cc9e465427b5a5b1efe309e685dc574638c
Max Murphy-Skvorzov <max@winning.black>
2023-02-23 02:39:48 +0100

    Bump default version

--- bin/versions.bash
+++ bin/versions.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034 # VAriables are expected to be unused in this file
-DFX_IC_COMMIT=08807e6c49348791dc6cad016a565f3882ec4ad6
+DFX_IC_COMMIT=ced285287d5513832ba8c66fc3793e438c74b531
