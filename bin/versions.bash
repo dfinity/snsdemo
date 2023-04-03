dc29ac8c80013809bece0ca3dafc7ca04ad5e925
Max Murphy-Skvorzov <max@winning.black>
2023-04-03 21:14:03 +0200

    Bump the ic commit to the latest published version that works with the current code

--- bin/versions.bash
+++ bin/versions.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034 # VAriables are expected to be unused in this file
-DFX_IC_COMMIT=aebee24076c67a57ff638d9eb9fe4a1d7561850d
+DFX_IC_COMMIT=204fed96ed7932e50ca5a771c768289e78664c0d
