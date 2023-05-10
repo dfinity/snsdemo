68fa37e7ae83a802430a1c55229f7daff424a09a
Max Murphy-Skvorzov <max@winning.black>
2023-05-10 11:53:13 +0200

    Specify a pinned version

--- bin/versions.bash
+++ bin/versions.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034 # VAriables are expected to be unused in this file
 DFX_IC_COMMIT=204fed96ed7932e50ca5a771c768289e78664c0d
+SNS_AGGREGATOR_RELEASE=proposal-115273-agg
 INTERNET_IDENTITY_RELEASE=release-2023-04-12
