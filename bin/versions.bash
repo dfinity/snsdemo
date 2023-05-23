3d18104a5315c2c5d655514cb6ff1be8f318e6d9
David de Kloet <david.de.kloet@dfinity.org>
2023-05-23 16:56:34 +0200

    Bump IC commit

--- bin/versions.bash
+++ bin/versions.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034 # VAriables are expected to be unused in this file
 SNS_AGGREGATOR_RELEASE=proposal-115273-agg
-DFX_IC_COMMIT=89129b8212791d7e05cab62ff08eece2888a86e0
+# Commit from 2023-05-17
+DFX_IC_COMMIT=5a285c40c505b103cb4d205f33e1d846eb9ed74b
 INTERNET_IDENTITY_RELEASE=release-2023-04-12
 NNS_DAPP_RELEASE=proposal-121690
