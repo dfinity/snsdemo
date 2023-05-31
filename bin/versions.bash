f43e435bb8f996fa0c0fb49199ca1f409ff03434
David de Kloet <david.de.kloet@dfinity.org>
2023-05-31 16:10:44 +0200

    revert ic commit

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,8 +2,8 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2023-05-30 which includes geo restriction fields
 SNS_AGGREGATOR_RELEASE=proposal-122625-agg
-# Commit from 2023-05-17 which includes simulate_manage_neuron
-DFX_IC_COMMIT=b9fc66eafca530e997313aa68aaac31d41e6a875
+# Commit from 2023-05-17
+DFX_IC_COMMIT=5a285c40c505b103cb4d205f33e1d846eb9ed74b
 INTERNET_IDENTITY_RELEASE=release-2023-04-12
 NNS_DAPP_RELEASE=proposal-121690
 DIDC_VERSION=2022-11-17
