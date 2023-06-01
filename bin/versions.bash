5ddced478afa9e6a51cbe9da6b589889c327a5e2
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2023-06-01 15:05:34 +0200

    snsdemo: Bump sns_aggregator release (#180)
    # Motivation
    
    Facilitate testing locally with geo restriction fields.
    
    # Changes
    
    1. Bump sns_aggregator release to include geo restriction fields.

--- bin/versions.bash
+++ bin/versions.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2034 # VAriables are expected to be unused in this file
-SNS_AGGREGATOR_RELEASE=proposal-115273-agg
+# shellcheck disable=SC2034 # Variables are expected to be unused in this file
+# Release from 2023-05-30 which includes geo restriction fields
+SNS_AGGREGATOR_RELEASE=proposal-122625-agg
 # Commit from 2023-05-17
 DFX_IC_COMMIT=5a285c40c505b103cb4d205f33e1d846eb9ed74b
 INTERNET_IDENTITY_RELEASE=release-2023-04-12
