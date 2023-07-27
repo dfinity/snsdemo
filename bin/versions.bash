7379c0080fe7eec86e3169133d23630cd169803a
Max Murphy-Skvorzov <max@winning.black>
2023-07-27 13:50:16 +0200

    Bump IC commit

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,8 +2,8 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2023-05-30 which includes geo restriction fields
 SNS_AGGREGATOR_RELEASE=proposal-123011-agg
-# Commit from 2023-07-04
-DFX_IC_COMMIT=78159ede9b7695f6a0e77b5ea53a55f67f044eb4
+# Commit from 2023-07-26
+DFX_IC_COMMIT=825e26f2c1f4cfdbe441f2bce3e2e5ba596d794b
 INTERNET_IDENTITY_RELEASE=release-2023-04-12
 NNS_DAPP_RELEASE=proposal-121690
 DIDC_VERSION=2023-07-25
