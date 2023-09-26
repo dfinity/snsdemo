5f4380b16bd800cb15867e54bcf7af68c660bb98
Max Murphy <max@winning.black>
2023-09-26 05:34:12 +0200

    Rm incorrect comment

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,7 +2,6 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2023-05-30 which includes geo restriction fields
 SNS_AGGREGATOR_RELEASE=proposal-123011-agg
-# Commit from 2023-07-04
 DFX_IC_COMMIT=4918bb79b1ff24defeec0d596c60796688b5ddec
 INTERNET_IDENTITY_RELEASE=release-2023-04-12
 NNS_DAPP_RELEASE=proposal-121690
