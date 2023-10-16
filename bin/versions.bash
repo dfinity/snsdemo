9cb76e2c6992a0e517f7c07b819014274a1c28d0
sa-github-api <138766536+sa-github-api@users.noreply.github.com>
2023-10-16 13:31:22 +0200

    Update IC commit (#240)
    Automated changes by
    [create-pull-request](https://github.com/peter-evans/create-pull-request)
    GitHub action
    
    Co-authored-by: gix-bot <gix-bot@users.noreply.github.com>

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2023-05-30 which includes geo restriction fields
 SNS_AGGREGATOR_RELEASE=proposal-123011-agg
-DFX_IC_COMMIT=dc5d8cfec5550f99dc4912501037478de8ab9426
+DFX_IC_COMMIT=2d57e93dabc5f13258d0dee1ffb2363ddce7fe62
 INTERNET_IDENTITY_RELEASE=release-2023-04-12
 NNS_DAPP_RELEASE=proposal-121690
 DIDC_VERSION=2023-07-25
