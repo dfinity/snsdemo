01cdefc0a6c41ec438145e41236f32b60b57a0ae
Llorenç <llorenc.muntaner@dfinity.org>
2023-10-19 14:45:26 +0200

    Chore: upgrade IC commit

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2023-05-30 which includes geo restriction fields
 SNS_AGGREGATOR_RELEASE=proposal-123011-agg
-DFX_IC_COMMIT=2d57e93dabc5f13258d0dee1ffb2363ddce7fe62
+DFX_IC_COMMIT=dd51544944987556c978e774aa7a1992e5c11542
 INTERNET_IDENTITY_RELEASE=release-2023-04-12
 NNS_DAPP_RELEASE=proposal-121690
 DIDC_VERSION=2023-07-25
