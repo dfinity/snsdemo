fcb5050a1106ca83c9d2c71c780885ac5842ed33
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-04-19 10:30:55 +0200

    Update ic version

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2023-05-30 which includes geo restriction fields
 SNS_AGGREGATOR_RELEASE=proposal-125509-agg
-DFX_IC_COMMIT=5997e7ba67707089dc25df45587b38ed26b401f3
+DFX_IC_COMMIT=3343a9ec1ea3170dcfd0cc4f4298d5ce09abb036
 INTERNET_IDENTITY_RELEASE=release-2023-10-27
 NNS_DAPP_RELEASE=proposal-121690
 DIDC_VERSION=2023-07-25
