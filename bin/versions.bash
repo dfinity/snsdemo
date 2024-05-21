e2fd6d78c61aa1e86299eef1c3e91ecc50f20392
David de Kloet <david.de.kloet@dfinity.org>
2024-05-21 15:01:41 +0200

    ee6addc1f

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2023-05-30 which includes geo restriction fields
 SNS_AGGREGATOR_RELEASE=proposal-125509-agg
-DFX_IC_COMMIT=1cb8b5d0d138112d67624aa85c534f81a8807369
+DFX_IC_COMMIT=ee6addc1f2775c7e78c82dcfda18fad2289bb8fd
 INTERNET_IDENTITY_RELEASE=release-2023-10-27
 NNS_DAPP_RELEASE=proposal-121690
 DIDC_VERSION=2023-07-25
