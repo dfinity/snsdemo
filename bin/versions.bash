e1a0927898e5ca51c381bfa1d01e0ba2ac7ea0fa
David de Kloet <david.de.kloet@dfinity.org>
2024-05-22 08:42:10 +0200

    Retry health check if not successful immediately

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2023-05-30 which includes geo restriction fields
 SNS_AGGREGATOR_RELEASE=proposal-125509-agg
-DFX_IC_COMMIT=9d21f31983eff399ffe369847b5fcbf0b1b944de
+DFX_IC_COMMIT=220d305aaad73454c5db419ab29265a1e6a360c0
 INTERNET_IDENTITY_RELEASE=release-2023-10-27
 NNS_DAPP_RELEASE=proposal-121690
 DIDC_VERSION=2023-07-25
