c0a526fe2b5a4a73c75d9923d699e3a55c15341a
David de Kloet <david.de.kloet@dfinity.org>
2024-06-17 10:52:21 +0200

    Update IC commit in snsdemo

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2023-05-30 which includes geo restriction fields
 SNS_AGGREGATOR_RELEASE=proposal-125509-agg
-DFX_IC_COMMIT=1c221e6c4c1fe8fedd039505dd46760e24af7b22
+DFX_IC_COMMIT=416058ebefaccb09c7056e6151fc72fcbae5a1ba
 INTERNET_IDENTITY_RELEASE=release-2023-10-27
 NNS_DAPP_RELEASE=proposal-121690
 DIDC_VERSION=2023-07-25
