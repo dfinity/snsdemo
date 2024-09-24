889af6e46f621f0dd2bf53f938da8ac5e7a265ca
sa-github-api <138766536+sa-github-api@users.noreply.github.com>
2024-09-24 09:09:42 +0200

    Update IC commit (#404)
    Automated changes by
    [create-pull-request](https://github.com/peter-evans/create-pull-request)
    GitHub action
    
    Co-authored-by: gix-bot <gix-bot@users.noreply.github.com>

--- bin/versions.bash
+++ bin/versions.bash
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034 # Variables are expected to be unused in this file
 # Release from 2024-05-02 which includes nervous_system_parameters.
 SNS_AGGREGATOR_RELEASE=proposal-129614-agg
-DFX_IC_COMMIT=e604b9899c36e3bbf94b330572b2f19563905d6c
+DFX_IC_COMMIT=285a5db07da50a4e350ec43bf3b488cc6fe36102
 INTERNET_IDENTITY_RELEASE=release-2023-10-27
 NNS_DAPP_RELEASE=proposal-121690
 DIDC_VERSION=2023-07-25
